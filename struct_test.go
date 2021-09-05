3105c69cce085ae7449d515e52418bdb4aa5fdcf
Xuanwo <github@xuanwo.io>
2021-09-05 15:57:02 +0800

    feat: Add test case for struct
    Signed-off-by: Xuanwo <github@xuanwo.io>

---
+++ struct_test.go
@@ -0,0 +1,33 @@
+package gg
+
+import "testing"
+
+func TestStruct(t *testing.T) {
+	t.Run("empty", func(t *testing.T) {
+		buf := pool.Get()
+		defer buf.Free()
+
+		expected := "type Test struct{}"
+
+		Struct("Test").render(buf)
+
+		compareAST(t, expected, buf.String())
+	})
+
+	t.Run("fields", func(t *testing.T) {
+		buf := pool.Get()
+		defer buf.Free()
+
+		expected := `type Test struct{
+A int64
+b string
+}`
+
+		Struct("Test").
+			Field("A", "int64").
+			Field("b", "string").
+			render(buf)
+
+		compareAST(t, expected, buf.String())
+	})
+}
